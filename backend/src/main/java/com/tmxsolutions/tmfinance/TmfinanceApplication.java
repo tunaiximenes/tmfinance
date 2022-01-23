e503af3d9b524a8f587c71f483cd10aff84a84aa
Tunai Ximenes <tunai.ximenes@gmail.com>
2022-01-23 01:52:25 -0300

    Primeiro commit

---
+++ backend/src/main/java/com/tmxsolutions/tmfinance/TmfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.tmxsolutions.tmfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmfinanceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tmxsolutions/tmfinance/TmfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmxsolutions.tmfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
